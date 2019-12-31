30c32dc4903fa4f43273d40271cef63494f2b4b0
frances-ho <thaiduyenh@gmail.com>
2019-12-30 22:04:04 -0500

    Add custom exception handler

--- PPMTool/src/main/java/com/ppmtool/services/ProjectService.java
+++ PPMTool/src/main/java/com/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.ppmtool.services;
 
 import com.ppmtool.domain.Project;
+import com.ppmtool.exceptions.ProjectIdException;
 import com.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+
     }
 }
